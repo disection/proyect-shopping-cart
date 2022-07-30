fcfd669cd5e5893e88acbf35e0881e3861866a6a
disection <diseccionmexico@gmail.com>
2022-07-29 21:27:00 -0500

    add folder pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col">
+          <nav class="navbar bg-dark">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">
+                <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+                Bootstrap
+              </a>
+            </div>
+          </nav>
+          </div>
+        </div>
+      </div>
         
     </div>
   );
