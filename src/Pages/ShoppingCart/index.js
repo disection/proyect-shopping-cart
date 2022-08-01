c018f1ff7370ac56b3154d76c782b52e5ef27300
disection <diseccionmexico@gmail.com>
2022-08-01 18:11:43 -0500

    ADD table list products on page shopping-cart

--- src/Pages/ShoppingCart/index.js
+++ src/Pages/ShoppingCart/index.js
@@ -1,4 +1,34 @@
 const ShoppingCart = () => {
-    return <h1>Carrito de Comparas</h1>
+    return (
+        <><h1>Carrito de Compras</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Producto</th>
+                        <th scope="col">Precio Unitario</th>
+                        <th scope="col">Cantidad</th>
+                        <th scope="col">Costo total</th>
+                    </tr>
+                </thead>
+                <tbody  className="table-group-divider">
+                    <tr>
+                        <td>Nombre de producto
+                            <button type="button" className="btn btn-outline-secondary  m-3 btn-sm">eliminar</button>
+                            <button type="button" className="btn btn-primary btn-sm">Comprar</button>
+                        </td>
+                        <td>$ 100.00</td>
+                        <td>2</td>
+                        <td>$ 200.00</td>
+                    </tr>
+                    
+                </tbody>
+                <tfoot>
+                    <tr>
+                       
+                        <th scope="col" colspan="4">Subtotal (1 producto): $4,209.66</th>
+                    </tr>
+                </tfoot>
+            </table></>
+    )
 }
 export default ShoppingCart
